905686c6a119b53fef416b94d2a3f53f1af413c8
jeffmoger <jeff-github@moger.com>
2020-04-16 13:23:48 -0700

    not needed

--- controllers/segment.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    getSegment(req, res, next) {
-      const segment = req.params.segment;
-      res.locals.segment = segment;
-      console.log('segment.js:' + res.locals.segment)
-      next()
-    }
-  }
